e0e1ba75573f933dd9d014499276050339f5216a
OINeptuneIO <zx1016433373@gmail.com>
2024-12-01 17:38:11 +0800

    10

--- prac_10/wiki.py
+++ prac_10/wiki.py
@@ -1,29 +1,21 @@
-
 import wikipedia
 
-def main():
-    while True:
-        # Prompt the user for input
-        search_input = input("Enter a page title or search phrase (press Enter to quit): ").strip()
+while True:
+    search_phrase = input("Enter a page title or search phrase (or leave blank to quit): ")
 
-        # If the user enters blank input, exit the loop
-        if not search_input:
-            break
+    if not search_phrase:
+        print("Exiting...")
+        break
 
-        try:
-            # Search for the page
-            page_title = wikipedia.search(search_input)[0]  # Assuming the first search result
-            page = wikipedia.page(page_title, auto_suggest=False)
-
-            # Print page title, summary, and URL
-            print("Title:", page.title)
-            print("Summary:", page.summary)
-            print("URL:", page.url)
-            print()
-        except wikipedia.exceptions.DisambiguationError as e:
-            print("Disambiguation Error: Please specify your search term further.")
-        except wikipedia.exceptions.PageError:
-            print("Page Error: The page does not exist.")
-
-if __name__ == "__main__":
-    main()
+    try:
+        page = wikipedia.page(search_phrase, autosuggest=False)
+        print("Title:", page.title)
+        print("Summary:", page.summary)
+        print("URL:", page.url)
+    except wikipedia.DisambiguationError as e:
+        print("Disambiguation Error: Wikipedia page for '{}' could refer to multiple topics. Please be more specific.".format(
+                search_phrase))
+    except wikipedia.PageError as e:
+        print("Page Error: Wikipedia page for '{}' does not exist.".format(search_phrase))
+    except Exception as e:
+        print("An error occurred:", e)
